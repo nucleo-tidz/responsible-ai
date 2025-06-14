3f0507adab6be17447b715be5edeaea8d871940e
ahmar-husain <ahmar.husain@maersk.com>
2025-06-14 19:33:17 +0530

    Send

---
+++ src/api/Controllers/ContentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContentController : ControllerBase
+    {
+        [HttpPost("add/to/block/list")]
+        public async Task<IActionResult> Add(string content)
+        {
+            return Created();
+        }
+    }
+}
--- src/infrastructure/DependencyInjection.cs
+++ src/infrastructure/DependencyInjection.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Azure;
+using Azure.AI.ContentSafety;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Net;
 
 namespace infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection Add(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddContentSafety(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddAzureClients(x =>
+            {
+                x.AddContentSafetyClient(new Uri(configuration["ContentSafety:URI"]), new AzureKeyCredential(configuration["ContentSafety:Key"]));
+            });
             return services;
         }
 
